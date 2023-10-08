7bc29ad83596c02212b2b03d14c6e9abc6d920fd
Mike Njuki <mikenjuki@gmail.com>
2023-10-08 17:40:47 +0300

    State change

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -12,6 +12,7 @@ interface SectionHeaderProps {}
 const SectionHeader: FC<SectionHeaderProps> = ({}) => {
   const activeStep = useStepStore((state) => state.activeStep);
   const setActiveStep = useStepStore((state) => state.setActiveStep);
+  const slides = useStepStore((state) => state.slides);
 
   const handlePrevStep = () => {
     setActiveStep(Math.max(activeStep - 1, 0));
@@ -33,7 +34,7 @@ const SectionHeader: FC<SectionHeaderProps> = ({}) => {
       </Button>
       <ContentSteps />
       <Button
-        disabled={activeStep === 3}
+        disabled={activeStep === slides.length - 1}
         variant={"ghost"}
         className="disabled:bg-transparent text-2xl disabled:text-[#4a4e54] btn-circle"
         onClick={handleNextStep}
